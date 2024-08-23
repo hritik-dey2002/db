a1aa7e3a97e160ab5c5de0f7bdb19db4b44f03ca
Hritik Dey <110844175+hritik-dey2002@users.noreply.github.com>
2024-08-24 00:47:31 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,235 @@
+import mysql.connector
+import streamlit as st
+
+
+
+connection = mysql.connector.connect(
+        host='sql12.freesqldatabase.com',
+        user='sql12727496',
+        password='bDAhbMxUy3',
+        database='sql12727496'
+    )
+cursor = connection.cursor()
+print("Connection successful")
+
+ad_user = st.text_input("Username")
+ad_password = st.text_input("Password")
+
+def insert_data(id,name):
+    DB_table_name = 'user'
+    insert_sql = "insert into " + DB_table_name + """
+    values (%s,%s)"""
+    rec_values = (id,name)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+def run():
+    if st.button("submit resume", type="primary"):
+        insert_data(ad_user,ad_password)
+
+run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+# import spacy
+# import nltk
+# spacy.load('en_core_web_sm')
+# nltk.download('stopwords')
+# import pandas as pd
+# import time, datetime
+# from pyresparser import ResumeParser
+# from pdfminer3.layout import LAParams, LTTextBox
+# from pdfminer3.pdfpage import PDFPage
+# from pdfminer3.pdfinterp import PDFResourceManager
+# from pdfminer3.pdfinterp import PDFPageInterpreter
+# from pdfminer3.converter import TextConverter
+# from streamlit_tags import st_tags
+# from PIL import Image
+# import plotly.express as px
+# from plotly import optional_imports
+# # from pdfminer.high_level import extract_text
+
+# # connection = pymysql.connect(host='localhost', user='root', password='')
+# # cursor = connection.cursor()
+
+# def run():
+#     st.title("Smart Resume Screening")
+#     activities = ["User", "Admin","Company"]
+#     choice = st.selectbox("Choose among the given options:", activities)
+#     # img = Image.open('./Logo/smart.jpg')
+#     # img = img.resize((700, 390))
+#     # st.image(img)
+
+   
+#     if choice == 'User':
+
+#             pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+#             if st.button("submit resume", type="primary"):
+#                 if pdf_file is not None:
+#                     # save_image_path = './Uploaded_Resumes/' + pdf_file.name
+#                     # with open(save_image_path, "wb") as f:
+#                     #     f.write(pdf_file.getbuffer())
+#                     # resume_data = ResumeParser(save_image_path).get_extracted_data()
+#                     resume_data = ResumeParser(pdf_file).get_extracted_data()
+#                     # text = extract_text(save_image_path)
+#                     # st.text(text)
+#                     if resume_data:
+#                         st.header("*Resume Analysis*")
+#                         st.success("Congratulations " + resume_data['name'] +' . your resume has been submitted')
+#                         st.subheader("*Candidates Basic info*")
+#                         try:
+#                             st.text('Name: ' + resume_data['name'])
+#                             st.text('Email: ' + resume_data['email'])
+#                             st.text('Resume pages: ' + str(resume_data['no_of_pages']))
+#                         except:
+#                             pass
+
+#                         ts = time.time()
+#                         cur_date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+#                         cur_time = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+#                         timestamp = str(cur_date + '_' + cur_time)
+
+#                         st.balloons()
+#                 else:
+#                     st.warning('Warning message')
+            
+
+
+
+
+
+#     elif choice=='Admin':
+#         st.success('Welcome to Admin Side')
+#         admin_user=st.text_input("username")
+#         admin_password=st.text_input("password",type='password')
+#         if st.button('login'):
+#             if admin_user=='admin' and admin_password=='admin123':
+#                 st.success("welcome admin")
+#                 st.balloons()
+#             else:
+#                 st.error("Wrong ID & Password Provided")
+
+
+
+
+
+
+#     else:
+#         st.header("welcome to company side")
+#         admin_user=st.text_input("username")
+#         admin_password=st.text_input("password",type='password')
+#         if st.button('login'):
+#             if admin_user=='company' and admin_password=='company123':
+#                 st.success("welcome company")
+#                 st.balloons()
+#                 keywords = st_tags(
+#                     label='# Enter Keywords:',
+#                     text='Press enter to add more',
+#                     value=['Zero', 'One', 'Two'],
+#                     suggestions=['five', 'six', 'seven', 
+#                         'eight', 'nine', 'three', 
+#                         'eleven', 'ten', 'four'],
+#                     maxtags = 4,
+#                     key='1')
+#             else:
+#                 st.error("Wrong ID & Password Provided")
+
+# run()
